c5249363963bc4093c50a4c4557d8bd8c5015116
lecielMon <986354147@qq.com>
2023-01-30 14:53:04 +0800

    note chapter01

--- chapter01/main.py
+++ chapter01/main.py
@@ -13,17 +13,53 @@ import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 card = Card(suit='A', rank='heart')
+
 # 从序列中随机选出一个元素
 import random
 
 attr = random.choice(range(100))
+
 # 反向迭代
 for i in reversed(range(10)):
     pass
+
 # __contains__ String类的方法，判断给定子字符串是否是字符串的一部分
 ret = 'a' in 'ha'
-# TODO 创建字典
-# TODO 创建列表
-# TODO 通过内置函数使用特殊方法，例子：二维向量的加法
+
+# 创建字典
+ka = {'a': 0, 'b': 1, 'c': 2}
+kb = dict(a=0, b=1, c=2)
+kc = dict([('a', 0), ('b', 1), ('c', 2)])
+kd = dict(zip(['a', 'b', 'c'], [0, 1, 2]))
+
+# 创建列表
+la = list('abcde')
+lb = ['a', 'b', 'c', 'd', 'e']
+lc = [i + 'a' for i in lb]
+ld = list(range(5))
+
+# 通过内置函数使用特殊方法，例子：二维向量的加法
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r,%r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+
+v1 = Vector(3, 4)
+v2 = Vector(3, 4)
+
 # TODO 83个特殊方法
-# test
